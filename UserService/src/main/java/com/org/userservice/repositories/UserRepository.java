483f72ee8be6dab2f1052349de039c85358b6701
Anoop <anoop.pandey8@gmail.com>
2022-12-26 20:51:49 +0530

    remove unwanted code

--- UserService/src/main/java/com/org/userservice/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.userservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.userservice.entities.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
